ebe65dfb439b354219ce085d75333453184a4a59
David Gonzalez <david.dg.gonzalez@etu.hesge.ch>
2016-11-21 18:54:21 +0100

    review together

--- src/file_linker.c
+++ src/file_linker.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include "file_linker.h"
 
+//TODO return state to pass to next call to file_linker_update
 void file_linker_update(char *dst_path, finder_files_t *files) {
     (void)dst_path;
-    (void)files;
 
     for (uint i; i < files->count; i++) {
         fprintf(stdout, "File found: %s\n", files->files[i]);
