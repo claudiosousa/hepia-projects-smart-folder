d0b58d9e30aca923806e33b539ee3d43e69d68ec
Claudio <claudio.sousa@gmail.com>
2016-11-24 20:51:09 +0100

    file

--- src/file_validator.c
+++ src/file_validator.c
@@ -5,6 +5,28 @@ struct file_validator_t
 
 };
 
+// name exact:    -name  <filename>
+// name contains: -name -<filename>
+// group exact: -group root
+// user exact: -user claudio
+// perm min: -perm -000
+// perm exact: -perm 000
+// size: -size (+-)n(GMkc)
+// format numerique: n, +n,-name
+
+//DATE (ref NOW):
+// access time minutes: -atime (+-)<minutes>m
+// access time days:    -atime (+-)<days>d
+// modified time minutes/days: -mtime (+-)<minutes>m
+// creation time minutes/days: -ctime (+-)<minutes>m
+
+//operators:
+// negation: -not
+// and: -and
+// or: -or
+// expr: '\(...\)'
+
+
 file_validator_t *file_validator_create(char *expression[], size_t size)
 {
     (void)expression;
